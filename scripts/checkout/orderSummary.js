f83a155b1c83904ddcebd732a955b0761ce3e5b2
ilhambonardoo <ilhambonardo123@gmail.com>
2025-08-13 17:35:35 +0700

    create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -25,17 +25,17 @@ export function renderOrderSummary() {
     const dateString = deliveryDate.format("MMMM D, YYYY");
 
     cartSummaryHTML += `
-    <div class="cart-item-container js-cart-item-container-${productId}">
+    <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date : ${dateString}</div>
       <div class="cart-item-details-grid">
         <img class="product-image" src="${matchingProduct.image}" />
         <div class="cart-item-details">
         <div class="product-name">${matchingProduct.name}</div>
         <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
-        <div class="product-quantity">
+        <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span> </span>
             <span class="update-quantity-link link-primary js-update-link"> Update </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id = ${matchingProduct.id}> Delete </span>
+            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id = ${matchingProduct.id}> Delete </span>
         </div>
       </div>
       <div class="delivery-options">
@@ -99,4 +99,3 @@ export function renderOrderSummary() {
     });
   });
 }
-renderOrderSummary();
---
+++ test-jasmine/checkout/orderSummaryTest.js
@@ -0,0 +1,48 @@
+import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
+import { loadFromStorage, cart } from "../../data/cart.js";
+describe("test suite : RenderOrderSummary", () => {
+  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2 = "54e0eccd-8f36-462b-b68a-8182611d9add";
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
+
+    document.querySelector(".js-test-container").innerHTML = `
+    <div class="js-order-summary"></div>
+    <div class="js-payment-summary"></div>
+    <div class="js-header-container"></div>`;
+
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]);
+    });
+    loadFromStorage();
+    renderOrderSummary();
+  });
+
+  it("tampilan cart", () => {
+    expect(document.querySelectorAll(".js-cart-item-container").length).toEqual(2);
+    expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain("Quantity: 2");
+    expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain("Quantity: 1");
+    document.querySelector(".js-test-container").innerHTML = "";
+  });
+
+  it("test menghapus produk (delete)", () => {
+    expect(document.querySelector(`.js-delete-link-${productId1}`).click());
+    expect(document.querySelectorAll(`.js-cart-item-container`).length).toEqual(1);
+    expect(document.querySelector(`.js-cart-item-container-${productId1}`)).toEqual(null);
+    expect(document.querySelector(`.js-cart-item-container-${productId2}`)).not.toEqual(null);
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual(productId2);
+    document.querySelector(".js-test-container").innerHTML = "";
+  });
+});
